de56f267a0c794961b35ce634b6b505262b3c678
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-06-23 14:38:54 -0300

    Migracion y carga de clientes a traves de un json

--- Dsw2025Tpi.Api/Program.cs
+++ Dsw2025Tpi.Api/Program.cs
@@ -1,4 +1,11 @@
 
+using Dsw2025Tpi.Data;
+using Dsw2025Tpi.Data.Helpers;
+using Dsw2025Tpi.Data.Repositories;
+using Dsw2025Tpi.Domain.Entities;
+using Dsw2025Tpi.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Dsw2025Tpi.Api;
 
 public class Program
@@ -14,6 +21,15 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddHealthChecks();
+        builder.Services.AddDbContext<Dsw2025TpiContext>(options => 
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Dsw2025TpiEntities"));
+            options.UseSeeding((c,t) => 
+            {
+                ((Dsw2025TpiContext)c).Seedwork<Customer>("customers.json");
+            });
+        });
+        builder.Services.AddScoped<IRepository,EfRepository>();
 
         var app = builder.Build();
 
--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
 public class Dsw2025TpiContext: DbContext
 {
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext>options):base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(p => p.Sku).IsRequired().HasMaxLength(20);
+            entity.HasIndex(p => p.Sku).IsUnique();
+            entity.Property(p => p.Name).IsRequired().HasMaxLength(60);
+            entity.Property(p => p.CurrentUnitPrice).IsRequired().HasPrecision(15,2);
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.Property(c => c.Email).IsRequired().HasMaxLength(150);
+            entity.Property(c => c.Name).IsRequired().HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasOne(o => o.Customer)
+                  .WithMany()
+                  .HasForeignKey(o => o.CustomerId);
+
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.Property(i => i.UnitPrice).IsRequired().HasPrecision(15, 2);
+            entity.Property(i => i.Quantity).IsRequired();
+
+            entity.HasOne(i => i.Product)
+                  .WithMany()
+                  .HasForeignKey(i => i.ProductId);
+
+            entity.HasOne(i => i.Order)
+                  .WithMany(o => o.OrderItems)
+                  .HasForeignKey(i => i.OrderId);
+        });
+    }
 }
