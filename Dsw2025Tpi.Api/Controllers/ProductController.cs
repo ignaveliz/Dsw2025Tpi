047ab90d199cb649d255ab3fc8f606bf5c27bf8d
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-06-25 22:20:26 -0300

    Agregue un controlador api para productos con 2 end points, uno para agregar productos y otro para obtener todos los productos

---
+++ Dsw2025Tpi.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Dsw2025Tpi.Application.Dtos;
+using Dsw2025Tpi.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Tpi.Api.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+    private readonly ProductsManagementService _service;
+    public ProductController(ProductsManagementService service)
+    {
+         _service = service;
+    }
+
+    [HttpPost()]
+    public async Task<IActionResult> AddProduct([FromBody]ProductModel.Request request)
+    {
+        try
+        {
+            var product = await _service.AddProduct(request);
+            return Ok(product);
+        }
+        catch (ArgumentException ae)
+        {
+            return BadRequest(ae.Message);
+        }
+        catch (ApplicationException de)
+        {
+            return Conflict(de.Message);
+        }
+        catch (Exception)
+        {
+            return Problem("Error al guardar el producto");
+        }
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await _service.GetProducts();
+        if (products == null || !products.Any()) return NoContent();
+        return Ok(products);
+    }
+}
