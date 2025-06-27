388b681d3eb951e21f5528f016262ff4037e9d4a
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-06-26 21:01:14 -0300

    Fixed: customers no se cargaban en la DB

--- Dsw2025Tpi.Data/Migrations/20250626234039_InitialCreate.Designer.cs
+++ Dsw2025Tpi.Data/Migrations/20250626234039_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dsw2025Tpi.Data.Migrations
 {
     [DbContext(typeof(Dsw2025TpiContext))]
-    [Migration("20250623150545_InitialCreate")]
+    [Migration("20250626234039_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
