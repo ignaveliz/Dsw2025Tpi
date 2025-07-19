3974062ef95307b00e2bf02c9a84ff25da883ac9
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-07-18 21:01:19 -0300

    Primera implementacion de autenticacion

---
+++ Dsw2025Tpi.Api/Controllers/AuthenticateController.cs
@@ -0,0 +1,58 @@
+﻿using Dsw2025Tpi.Application.Dtos;
+using Dsw2025Tpi.Application.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Tpi.Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticateController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly JwtTokenService _jwtTokenService;
+
+    public AuthenticateController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        JwtTokenService jwtTokenService)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _jwtTokenService = jwtTokenService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel request)
+    {
+        var user = await _userManager.FindByNameAsync(request.Username);
+        if (user == null)
+        {
+            return Unauthorized("Usuario o contraseña incorrectos");
+        }
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+        if (!result.Succeeded)
+        {
+            return Unauthorized("Usuario o contraseña incorrectos");
+        }
+
+        var token = _jwtTokenService.GenerateToken(request.Username);
+        return Ok(new { token });
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+
+
+        var user = new IdentityUser { UserName = model.Username, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        // Opcional: enviar email de confirmación, etc.
+        return Ok("Usuario registrado correctamente.");
+    }
+}
--- Dsw2025Tpi.Api/Controllers/OrderController.cs
+++ Dsw2025Tpi.Api/Controllers/OrderController.cs
@@ -1,11 +1,13 @@
 ﻿using Dsw2025Tpi.Application.Dtos;
 using Dsw2025Tpi.Application.Exceptions;
 using Dsw2025Tpi.Application.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2025Tpi.Api.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("api/orders")]
 public class OrderController : ControllerBase
 {
